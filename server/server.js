c2f40d7199d0e4f89dd50835a9a932ba4ccc815e
MarcManiez <marc.maniez@gmail.com[A>
2017-01-15 14:35:17 -0800

    Add a few more modules, start server and serve static assets in /client.

--- server/server.js
+++ server/server.js
@@ -1,2 +1,15 @@
+const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const port = 1337;
+const app = express();
+
+app.listen(port);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/../client'));
+
+console.log(__dirname);
