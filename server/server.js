18f1e4fb49d0c2177e987adbb6eea94751332cfd
MarcManiez <marc.maniez@gmail.com[A>
2017-01-15 18:05:14 -0800

    Add mongoose dependency and initialize mongodb.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/testDB');
+ 
+var db = mongoose.connection;
+ 
+db.on('error', function (err) {
+console.log('connection error', err);
+});
+db.once('open', function () {
+console.log('connected to MongoDB');
+});
 
 const port = 1337;
 const app = express();
-
 app.listen(port);
 
 app.use(bodyParser.urlencoded({extended: true}));
