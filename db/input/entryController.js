4c68b1d83a21186919447f4662eb5788b86413a4
MarcManiez <marc.maniez@gmail.com[A>
2017-01-15 20:05:15 -0800

    Add moment and format time correctly for mongoDB.

--- db/input/entryController.js
+++ db/input/entryController.js
@@ -1,10 +1,11 @@
 const Entry = require('./entryModel');
 const db = require('../../server/server');
+const moment = require('moment');
 
 module.exports = { 
   newLink: function (req, res, next) {
-    console.log('WE MADE IT!!?!');
-    const dob = new Date(req.body.year, req.body.month, req.body.day);
+    let dob = new Date(req.body.year, req.body.month, req.body.day);
+    dob = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
     const entry = new Entry({
       name: req.body.name,
       dob: dob
