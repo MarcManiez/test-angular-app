ed428e91ae85be97d7805349cf75c3cc9d17e017
MarcManiez <marc.maniez@gmail.com[A>
2017-01-15 16:54:19 -0800

    Add entry data to POST request.

--- client/app/factories.js
+++ client/app/factories.js
@@ -1,10 +1,11 @@
 angular.module('filterer.services', [])
 
 .factory('requests', $http => {
-  const postEntry = () => {
+  const postEntry = (entry) => {
     return $http({
       method: 'POST',
-      url: '/input'
+      url: '/input',
+      data: entry
     })
     .then(res => {
       return resp.data;
